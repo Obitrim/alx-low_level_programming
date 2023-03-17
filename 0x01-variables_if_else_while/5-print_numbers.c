92630fbd74b01f00156b99de3adcdb94eab59304
Obitrim <pevangelist@gmail.com>
2023-03-17 16:22:41 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal;
+
+	for (decimal = 0; decimal < 10; decimal++)
+	{
+		printf("%d", decimal);
+	}
+	printf("\n");
+
+	return (0);
+}
