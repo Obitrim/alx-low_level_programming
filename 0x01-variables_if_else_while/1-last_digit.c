5200d18d7589927bf0baeba20c032346bee440c9
Obitrim <pevangelist@gmail.com>
2023-03-17 16:10:54 -0700

    add whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is", n);
+		printf("Last digit of %d is ", n);
 		printf("%d and is less than 6 and not 0\n", lastDigit);
 	}
 	return (0);
