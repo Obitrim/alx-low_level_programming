1d2eedd1bb9108cdc77f4c9279d5ca6c35e68bb1
Obitrim <pevangelist@gmail.com>
2023-03-17 16:38:55 -0700

    print decimals

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal;
+
+	for (decimal = 48; decimal < 58; decimal++)
+	{
+		putchar(decimal);
+	}
+	putchar('\n');
+
+	return (0);
+}
