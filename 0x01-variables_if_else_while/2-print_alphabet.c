ef1258b31f1c039161e4897d2e6e1cca1915c068
Obitrim <pevangelist@gmail.com>
2023-03-17 16:09:15 -0700

    print lowercase chars

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	/**
+	* print letters in lowercase
+	*/
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
