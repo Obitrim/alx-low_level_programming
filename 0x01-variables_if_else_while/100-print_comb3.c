ebbe16b727a92545f78ff113bd80f39d78419571
Obitrim <pevangelist@gmail.com>
2023-03-17 17:58:02 -0700

    double combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 48; num1 < 58; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 58; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			if (num1 < 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
