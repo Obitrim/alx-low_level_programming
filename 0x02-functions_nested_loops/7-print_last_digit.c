be154e78accb6fee55f68ef053135681fac55b9e
Obitrim <pevangelist@gmail.com>
2023-03-21 08:09:06 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *
+ * @num: number whose last digit is to be returned
+ *
+ * Return: value of last digit
+ */
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	if (num < 0)
+		num = num  * -1;
+
+	last_digit = num % 10;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
