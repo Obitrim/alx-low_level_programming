815f01b1430fef77ecfac491165ee574a8a0bd0c
Obitrim <pevangelist@gmail.com>
2023-03-21 09:44:01 -0700

    sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sums two numbers
+ *
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: Sum of two numbers
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
