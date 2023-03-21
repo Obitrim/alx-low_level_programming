55f3b70637dd25f3aea37064696bd82f5644840d
Obitrim <pevangelist@gmail.com>
2023-03-21 08:42:09 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,34 @@
  *
  * Return: Always 0 (Success)
  */
-int jack_bauer(void)
+void jack_bauer(void)
 {
-	
+	int hours, minutes, i, j;
+
+	hours = 23;
+	minutes = 59;
+
+	for (i = 0; i <= hours; i++)
+	{
+		for (j = 0; j <= minutes; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			
+			if (i > 9)
+				_putchar((i / 10) + '0');
+
+			_putchar((i % 10) + '0');
+			_putchar(':');
+
+			if (j < 10)
+				_putchar('0');
+			
+			if (j > 9)
+				_putchar((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
