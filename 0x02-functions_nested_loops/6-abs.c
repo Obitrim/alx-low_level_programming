6672555c6be4129baeec5d678fc389ee2a7bc0fd
Obitrim <pevangelist@gmail.com>
2023-03-21 07:47:52 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - get absolute value
+ *
+ * @num: number to test
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int num)
+{
+	return num >= 0 ? num : num * -1;
+}
