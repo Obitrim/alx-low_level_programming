302cf61c970a1e93197a65c085bddfa3e4f5a180
Obitrim <pevangelist@gmail.com>
2023-03-23 14:16:28 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: number of \ to print
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		int j;
+
+		for (j = 1; j <= i; j++)
+		{
+			if(j < i)
+			{
+				_putchar(' ');
+				continue;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		
+	}
+}
