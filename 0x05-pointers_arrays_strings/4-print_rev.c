88f4f606ca41bea0633926f8116eee2dc0672bf5
Obitrim <pevangelist@gmail.com>
2023-03-27 15:28:30 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @s: string variable
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count > -1)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+	_putchar('\n');
+}
