8a2686c2ad6e1f6ce327553ceeb2e3fa80ad6901
Obitrim <pevangelist@gmail.com>
2023-03-30 12:18:50 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * cap_string - capitalize all words in a string
- *
+ * @str: string;
  * Return: str
  */
 char *cap_string(char *str)
@@ -16,8 +16,6 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
-			str[index - 1] == '+' ||
-			str[index - 1] == '-' ||
 			str[index - 1] == '.' ||
 			str[index - 1] == '!' ||
 			str[index - 1] == ';' ||
