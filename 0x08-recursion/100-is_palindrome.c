7bdcdbff0081ea1cc02505812a24147271cab75e
Obitrim <pevangelist@gmail.com>
2023-04-05 23:05:08 -0700

    fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@
 */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
@@ -16,7 +16,7 @@ int _strlen_recursion(char *s)
 * @s: string pointer
 * @n1: first number
 * @n2: second number
-* Return compares characters
+* Return: compares characters
 */
 int comparator(char *s, int n1, int n2)
 {
