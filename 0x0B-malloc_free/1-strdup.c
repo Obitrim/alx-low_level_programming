0e9a18fc99b74d8ac5790b35860810b1f6b97683
Obitrim <pevangelist@gmail.com>
2023-04-23 09:54:03 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate string at @str
+ * @str: pointer to string to duplicate
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	if (str == NULL)
+		return (NULL);
+
+	dup = (char *) malloc((strlen(str) + 1));
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(dup, str);
+	return (dup);
+}
