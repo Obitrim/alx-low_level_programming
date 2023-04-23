7d1fe5b3436443f2066acf60d9ceaf4554c2f2fb
Obitrim <pevangelist@gmail.com>
2023-04-23 09:36:03 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Entry point
+ * @size: size of array
+* @c: initialization character
+ * Return: Always 0 (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *block;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	block = (char *) malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		block[i] = c;
+	}
+
+	return (block);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,10 @@
+# DYNAMIC MEMORY ALLOCATION
+
+Allocate memory dynamically with
+
+[] - malloc
+[] - calloc
+[] - realloc
+
+Deallocate memory clock with
+[] - free
