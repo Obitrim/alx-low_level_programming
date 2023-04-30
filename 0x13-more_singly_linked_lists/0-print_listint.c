8e654143141b746d81606ed2229f56f77d6b4671
Obitrim <pevangelist@gmail.com>
2023-04-30 03:09:55 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_listint - prints all elements list
+* @h: head node
+*
+* Return: Number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count;
+
+	node_count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
---
+++ 0x13-more_singly_linked_lists/template.c
@@ -0,0 +1,6 @@
+/**
+*
+*
+*/
+size_t print_listint(const listint_t *h)
+{}
