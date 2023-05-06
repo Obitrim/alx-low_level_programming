8dd442f078e1488a1805da5cafe2e499611cf3b7
Obitrim <pevangelist@gmail.com>
2023-05-06 09:58:26 -0700

    add node to index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,19 +25,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		index++;
 		if (index == (idx - 1))
 		{
-			new_node = malloc(sizeof(listint_t));
-
-			if (new_node == NULL)
-			{
-				return (NULL);
-			}
-
-			new_node->n = n;
-			new_node->next = (*head)->next;
-			(*head)->next = new_node;
-
-			return (new_node);
+			break;
 		}
+	}
+
+	if (idx < index)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+		{
+			 return (NULL);
+		}
+
+		new_node->n = n;
+		new_node->next = (*head)->next;
+		(*head)->next = new_node;
+
+		return (new_node);
 	}
 
 	return (NULL);
